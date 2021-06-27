08f6c68292ed3acd5809f2ced74646ffe46f9242
baza04 <bazarbay.amir@gmail.com>
2021-06-27 13:44:30 +0600

    feat: added main.go

---
+++ cmd/web/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+
+	todoapp "github.com/baza04/todoApp"
+	"github.com/baza04/todoApp/pkg/handler"
+)
+
+func main() {
+	srv := new(todoapp.Server)
+	handler := new(handler.Handler)
+
+	if err := srv.Run("8000", handler.InitRouter()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
