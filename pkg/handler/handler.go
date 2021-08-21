4776a61d50582b9b62e00c3781ef26f5e0a6d571
baza04 <bazarbay.amir@gmail.com>
2021-08-21 21:58:14 +0600

    feat: added swagger

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -3,6 +3,10 @@ package handler
 import (
 	"github.com/baza04/todoApp/pkg/service"
 	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+
+	_ "github.com/baza04/todoApp/docs"
 )
 
 type Handler struct {
@@ -15,6 +19,8 @@ func NewHandler(services *service.Service) *Handler {
 
 func (h *Handler) InitRoutes() *gin.Engine {
 	router := gin.New()
+
+	router.GET("/api/doc/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	auth := router.Group("/auth")
 	{
